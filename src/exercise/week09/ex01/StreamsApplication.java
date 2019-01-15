c4556fad61d2653ef97b130f666aff1449824d86
nahayatim <mahtabnahayati@gmail.com>
2019-01-15 10:53:16 +0100

    exo2 streamsTest

--- src/exercise/week09/ex01/StreamsApplication.java
+++ src/exercise/week09/ex01/StreamsApplication.java
@@ -7,32 +7,31 @@ import java.util.stream.Collectors;
 public class StreamsApplication {
     public static void main(String[] args) {
 
-        // a list of the first twenty numbers
         List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i < 21; i++) {
             numbers.add(i);
         }
 
-        //a list of the even numbers
+
         List<Integer> evenNumbers = numbers.stream()
                 .filter(number -> number % 2 == 0)
                 .collect(Collectors.toList());
         System.out.println(evenNumbers);
 
-        //a list of the odd numbers
+
         List<Integer> oddNumbers = numbers.stream()
                 .filter(number -> number % 2 == 1)
                 .collect(Collectors.toList());
         System.out.println(oddNumbers);
 
-        // list of the numbers divisible by 3 and bigger than 10
+
         List<Integer> biggerThan = numbers.stream()
                 .filter(number -> number % 3 == 0)
                 .filter(number -> number > 10)
                 .collect(Collectors.toList());
         System.out.println(biggerThan);
 
-        //smaller than 5 and multiply them by 3
+        
         List<Integer> smallerthan = numbers.stream()
                 .filter(e -> e < 5)
                 .map(e -> e * 3)
--- src/exercise/week09/ex02/StreamsTest.java
+++ src/exercise/week09/ex02/StreamsTest.java
@@ -1,4 +1,68 @@
 package exercise.week09.ex02;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamsTest {
+
+    private List<String> names = new FileReader().asList("exercise/week09/ex02/names.txt");
+
+
+    @Test
+    void testShortName() {
+        List<String> shorterName = names.stream()
+                .filter(e -> e.length() < 4)
+                .collect(Collectors.toList());
+        List<String> expected = Arrays.asList("El");
+
+        Assertions.assertEquals(expected, shorterName);
+
+
+    }
+
+    @Test
+    void testNameEndWithM() {
+        List<String> nameEndWithM = names.stream()
+                .filter(e -> e.endsWith("m"))
+                .map(e -> e.toUpperCase())
+                .collect(Collectors.toList());
+
+
+        List<String> expected = Arrays.asList("HOSAM", "TAMMAM");
+        Assertions.assertEquals(expected, nameEndWithM);
+    }
+
+
+    @Test
+    void testNameContainEandR() {
+        List<String> nameContainER = names.stream()
+                .filter(e -> e.contains("e"))
+                .filter(e -> e.contains("r"))
+                .collect(Collectors.toList());
+
+        List<String> expected = Arrays.asList("Mehran", "Norbert", "Serife");
+        Assertions.assertEquals(expected, nameContainER);
+
+    }
+
+    @Test
+    void testNameFourChar() {
+        List<String> testNameWithFourLetter = names.stream()
+                .filter(e -> e.length() == 4)
+                .map(e -> e.toLowerCase())
+                .filter(e -> e.contains("a"))
+                .filter(e -> e.contains("m"))
+                .map(e -> e.replace("a", "aa"))
+                .collect(Collectors.toList());
+
+        List<String> expected = Arrays.asList("maarj", "aamin", "omaar");
+
+        Assertions.assertEquals(expected, testNameWithFourLetter);
+    }
+
+
 }
