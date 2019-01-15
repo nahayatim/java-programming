bdb472663950a991f086410562533301f3ed5ff9
nahayatim <mahtabnahayati@gmail.com>
2019-01-15 18:05:24 +0100

    CaesarTest

--- src/exercise/week09/ex03/CaesarTest.java
+++ src/exercise/week09/ex03/CaesarTest.java
@@ -1,5 +1,33 @@
+package exercise.week09.ex03;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CaesarTest {
+
+    private Caesar caesar=new Caesar();
+
+    @Test
+    void testBigLetter(){
+        String hello = caesar.cipher("HELLO", 1);
+        String expected = "IFMMP";
+        Assertions.assertEquals(expected,hello);
+    }
+
+    @Test
+    void testSmallLetterWithSymbols(){
+        String helloWorld = caesar.cipher("hello, world", 13);
+        String expected="uryyb, jbeyq";
+        Assertions.assertEquals(expected,helloWorld);
+    }
+
+    @Test
+    void testSentence(){
+        String sentence =caesar.cipher("be sure to drink your Ovaltine",13);
+        String expected="or fher gb qevax lbhe Binygvar";
+        Assertions.assertEquals(expected,sentence);
+    }
 
 }
