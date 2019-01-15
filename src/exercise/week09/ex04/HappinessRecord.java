858756e5a775a391d11d599c6e9ee2f2aacb0886
nahayatim <mahtabnahayati@gmail.com>
2019-01-15 22:21:52 +0100

    happinessRecord class

--- src/exercise/week09/ex04/HappinessRecord.java
+++ src/exercise/week09/ex04/HappinessRecord.java
@@ -1,4 +1,34 @@
 package exercise.week09.ex04;
 
+
 public class HappinessRecord {
+
+    private String country;
+    private Integer rank;
+    private Double score;
+
+    public HappinessRecord(String country, Integer rank, Double score) {
+        this.country = country;
+        this.rank = rank;
+        this.score = score;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "Rank: " + rank +
+                "| Country: " + country +
+                "| Score: " + score;
+    }
 }
