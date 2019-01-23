e91843e6695eb45ec53322fa0d7e0a88c7186bec
nahayatim <mahtabnahayati@gmail.com>
2019-01-23 16:17:26 +0100

    add sms reader class

--- src/exercise/week10/ex03/SmsReader.java
+++ src/exercise/week10/ex03/SmsReader.java
@@ -1,4 +1,21 @@
 package exercise.week10.ex03;
 
+import exercise.week09.ex02.FileReader;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SmsReader {
+
+    public List<Sms> getSms() {
+
+        return new FileReader().asStream("exercise/week10/ex03/sms.csv")
+                .skip(1)
+                .map(line -> line.split(";"))
+                .map(columns -> new Sms(
+                        columns[0],
+                        (columns[1])
+                ))
+                .collect(Collectors.toList());
+    }
 }
