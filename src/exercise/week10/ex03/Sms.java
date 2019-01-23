59f16af32b5115564e445ac443f7c618ed3c3a10
nahayatim <mahtabnahayati@gmail.com>
2019-01-23 16:16:05 +0100

    add sms class

--- src/exercise/week10/ex03/Sms.java
+++ src/exercise/week10/ex03/Sms.java
@@ -1,4 +1,27 @@
 package exercise.week10.ex03;
 
 public class Sms {
+
+    private String label;
+    private String text;
+
+    public Sms(String v1, String v2) {
+        this.label = v1;
+        this.text = v2;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return "Sms" +
+                "label=" + label +
+                " text=" + text;
+    }
 }
