e585fc8893247067773e7c8842b2b32a22530437
nahayatim <mahtabnahayati@gmail.com>
2019-01-30 13:40:08 +0100

    add ex2-militarytime

---
+++ src/exercise/week11/ex02/MilitaryTimeTest.java
@@ -0,0 +1,12 @@
+package exercise.week11.ex02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MilitaryTimeTest {
+
+    @Test
+    void toMilitaryTime() {
+    }
+}
