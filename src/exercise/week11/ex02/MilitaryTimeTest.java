8e5bc45240bfefdbe718ec697c16b1489b41102c
nahayatim <mahtabnahayati@gmail.com>
2019-01-30 13:58:51 +0100

    add militarytest

--- src/exercise/week11/ex02/MilitaryTimeTest.java
+++ src/exercise/week11/ex02/MilitaryTimeTest.java
@@ -1,12 +1,35 @@
 package exercise.week11.ex02;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class MilitaryTimeTest {
 
+    private MilitaryTime militaryTime = new MilitaryTime();
+
     @Test
-    void toMilitaryTime() {
+    void testMidnight() {
+
+        String midnight = militaryTime.toMilitaryTime("12:00:00 AM");
+        String expected = "00:00:00";
+        Assertions.assertEquals(expected, midnight);
     }
+
+    @Test
+    void testNoon() {
+
+        String noon = militaryTime.toMilitaryTime("12:00:00 PM");
+        String expected = "12:00:00";
+        Assertions.assertEquals(expected, noon);
+    }
+
+    @Test
+    void testSevenInTheAfternoon() {
+
+        String seven = militaryTime.toMilitaryTime("07:05:45 PM");
+        String expected = "19:05:45";
+        Assertions.assertEquals(expected, seven);
+    }
+
 }
