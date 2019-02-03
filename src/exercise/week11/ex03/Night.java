768286bbead316dd9c1775055868ea73580cbb6f
nahayatim <mahtabnahayati@gmail.com>
2019-02-03 21:05:13 +0100

    extend class moment

--- src/exercise/week11/ex03/Night.java
+++ src/exercise/week11/ex03/Night.java
@@ -1,4 +1,10 @@
 package exercise.week11.ex03;
 
-public class Night {
+import java.time.LocalTime;
+
+public class Night extends Moment {
+
+    public Night() {
+        super(LocalTime.of(20, 01), LocalTime.of(5, 59), "at night");
+    }
 }
