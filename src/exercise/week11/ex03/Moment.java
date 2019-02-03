e590c5921b8ba69cc8d2d7a08bfbc70b4ca62933
nahayatim <mahtabnahayati@gmail.com>
2019-02-03 21:03:22 +0100

    abstract class

--- src/exercise/week11/ex03/Moment.java
+++ src/exercise/week11/ex03/Moment.java
@@ -1,4 +1,27 @@
 package exercise.week11.ex03;
 
-public class Moment {
+import java.time.LocalTime;
+
+public abstract class Moment {
+
+    private LocalTime start;
+    private LocalTime end;
+    private String name;
+
+    public Moment(LocalTime start, LocalTime end, String name) {
+        this.start = start;
+        this.end = end;
+        this.name = name;
+    }
+
+    public String getMoment() {
+        return name;
+    }
+
+    public Boolean isBetween(LocalTime currentTime) {
+        boolean isBetweenInterval = currentTime.isAfter(start) && currentTime.isBefore(end);
+        boolean isEqualInterval = currentTime.equals(start) || currentTime.equals(end);
+        return isBetweenInterval || isEqualInterval;
+    }
 }
+
