2d391875f7eec70fe2aeaa9325683ae1c4588cd8
nahayatim <mahtabnahayati@gmail.com>
2019-02-03 21:01:10 +0100

    extent class Time

--- src/exercise/week11/ex03/BiggerThan30.java
+++ src/exercise/week11/ex03/BiggerThan30.java
@@ -1,4 +1,24 @@
 package exercise.week11.ex03;
 
-public class BiggerThan30 {
+import java.util.Map;
+
+public class BiggerThan30 extends Time{
+
+        private Numbers numberToWord = new Numbers();
+        private Map<Integer, String> numbers = numberToWord.getNumbers();
+
+        @Override
+        public Boolean isInRange(Integer minute) {
+            if (minute > 30 && minute < 60) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String getMessage(Integer hour, Integer minute) {
+            return numbers.get(60-minute) + " to " + numbers.get(hour+1);
+
+        }
+
 }
