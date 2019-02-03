01b7ce6670a0d2104e97f72be9973bcd32e42175
nahayatim <mahtabnahayati@gmail.com>
2019-02-03 21:11:13 +0100

    list of moments

--- src/exercise/week11/ex03/Moments.java
+++ src/exercise/week11/ex03/Moments.java
@@ -1,4 +1,19 @@
 package exercise.week11.ex03;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Moments {
+
+    public static List<Moment> getMoment() {
+        return new ArrayList<>(Arrays.asList(
+                new Morning(),
+                new Noon(),
+                new Afternoon(),
+                new Evening(),
+                new Night(),
+                new Midnight()
+        ));
+    }
 }
