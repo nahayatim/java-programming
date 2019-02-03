eb550cba0401c9e8232bdbe2c421895e332b2605
nahayatim <mahtabnahayati@gmail.com>
2019-02-03 21:02:15 +0100

    list of Time

--- src/exercise/week11/ex03/Times.java
+++ src/exercise/week11/ex03/Times.java
@@ -1,4 +1,17 @@
 package exercise.week11.ex03;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Times {
+
+    public List<Time> getTimes() {
+        return new ArrayList<>(Arrays.asList(
+                new SmallerOrEquall30(),
+                new Oclock(),
+                new BiggerThan30()
+        ));
+    }
 }
+
