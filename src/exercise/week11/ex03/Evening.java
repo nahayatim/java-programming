a986b94c73e865eb865e382c1b232a25c68ec9a0
nahayatim <mahtabnahayati@gmail.com>
2019-02-03 21:04:07 +0100

    extend class moment

--- src/exercise/week11/ex03/Evening.java
+++ src/exercise/week11/ex03/Evening.java
@@ -1,4 +1,11 @@
 package exercise.week11.ex03;
 
-public class Evening {
+import java.time.LocalTime;
+
+public class Evening extends Moment {
+
+    public Evening() {
+
+        super(LocalTime.of(17, 00), LocalTime.of(20, 00), "in the evening");
+    }
 }
