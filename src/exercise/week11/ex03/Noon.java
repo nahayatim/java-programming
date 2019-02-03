859952e78d1cec42b401e452d90cd0e907c9d7ba
nahayatim <mahtabnahayati@gmail.com>
2019-02-03 21:15:33 +0100

    extend class moment

--- src/exercise/week11/ex03/Noon.java
+++ src/exercise/week11/ex03/Noon.java
@@ -1,4 +1,12 @@
 package exercise.week11.ex03;
 
-public class Noon {
+import java.time.LocalTime;
+
+public class Noon extends Moment {
+
+    //   At 12 o’clock, replace the whole message by noon
+
+    public Noon() {
+        super(LocalTime.of(00, 00), LocalTime.of(00, 00), "noon");
+    }
 }
